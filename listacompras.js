99b461f2dadb2a6d62d5e3b9dbb8d497996c0b50
fabuloso01 <fabuloso01@github.com>
2022-07-31 09:42:58 -0400

    commit

--- listacompras.js
+++ listacompras.js
@@ -3,15 +3,46 @@ let listaCompras = []
 
 function adicionarItem() {
  let item = document.getElementById("item").value
- listaCompras[listaCompras.length] = item
+ // listaCompras[listaCompras.length] = item
 
- limpar()
- imprimir()
+ if (item =="") {
+    alert("Campo item não preenchido!")
+ }else {
 
+    if(listaCompras.indexOf(item) < 0) {
+        listaCompras.push(item)
+        
+        imprimir()
+
+    } else {
+        alert("Item já adicionado!")
+
+    }
+    limpar()
+
+  }
+
+}
+
+function removerItem() {
+    let itemRemover = document.getElementById("itemRemover").value
+
+    let indiceRemover = listaCompras.indexOf(itemRemover)
+
+    if(indiceRemover < 0) {
+        alert("Item não encontrado!")
+    } else {
+        listaCompras.splice(indiceRemover, 1)
+        alert("Item removido com sucesso!")
+        imprimir()
+    }
+
+    limpar()
 }
 
 function limpar() {
     document.getElementById("item").value = ""
+    document.getElementById("itemRemover").value = ""
 }
 
 function imprimir() {
